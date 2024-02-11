8ff00be537e73043b23671337e269325c813ec14
“KocheshkovDmitry” <kocheshkovdmitry@gmail.com>
2024-02-11 17:12:30 -0600

    first

---
+++ print.py
@@ -0,0 +1,29 @@
+print('I am clever.')
+print("I'm student.")
+print("He, I am \"Vasya\"")
+
+print("I'm here.\nI'm at home.")
+
+print("I'm busy\\I'm at work")
+
+print("5")
+
+print(7)
+
+print(5 + 5, 5-3, 5*5, 5**2,"\n", 10/2, 17/2, 10//3, 3+3*2)
+
+
+print(10 % 20)
+
+print("Total: ", 5, 5)
+
+print("TOTAL: ", 5, 5, 5, sep="", end="+")
+print("TOTAL: ", 5, 5)
+
+print("min:", min(5, 4, -2, 0, -17, 20))
+print("max:", max(5, 4, -2, 0, -17, 20))
+print("moduls", abs(-29))
+print("stepen:", pow(5, 3), "cutarefm:", round(5 / 3))
+
+input("weight:")
+#("Hello:)
