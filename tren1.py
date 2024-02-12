df4a214876352e02d1fe1e0d5b4f1b1cdc69e40b
“KocheshkovDmitry” <kocheshkovdmitry@gmail.com>
2024-02-11 18:37:28 -0600

    two learn

---
+++ tren1.py
@@ -0,0 +1,19 @@
+w1 = int(input("Weight1:"))
+w2 = int(input("Weight2:"))
+
+w1 = w1 - w2
+w1 = w1 + 5
+w1 -= 5
+w1 *= 2
+w1 /= 2
+
+print("Resalts: ", w1 + w2)
+print("Resalts: ", w1 - w2)
+print("Resalts: ", w1 * w2)
+print("Resalts: ", w1 / w2)
+print("Resalts: ", w1 // w2)
+print("Resalts: ", w1 ** w2)
+
+a1 = "were be or "
+
+print(a1 * 5)
---
+++ variables.py
@@ -0,0 +1,26 @@
+number = (5)
+
+#print("resalts:", number)
+
+number = (17)
+
+#print("resalts:", number)
+
+number = (10) #int
+digital = (4.4545) #float
+world = ("resalts: ") #str
+figure = ("7 ") #str
+worldfigure = ("resalts: 50 ") #str
+bool = True #bool or False
+
+print(world, number)
+print(world + str(number))
+print(world + figure)
+print(world + figure + worldfigure)
+print(number + digital)
+print(digital + float(figure))
+print(world + str(number + int(figure)))
+print(worldfigure + str(number))
+
+
+
